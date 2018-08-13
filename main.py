d138b7a18597c0ffb8e4fabd95823b0e84062f84
Jack Huang <w305jack@gmail.com>
2018-08-14 00:54:58 +0800

    adjust main.py

--- main.py
+++ main.py
@@ -93,6 +93,5 @@ def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
 app.jinja_env.filters['datetime'] = datetimeformat
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
     app.jinja_env.auto_reload = True
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=8080)
