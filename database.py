c9328a5c469dd1476d6ddee4e2ffaa305d542e91
Jack Huang <w305jack@gmail.com>
2018-08-14 00:51:37 +0800

    adjust app.yaml

--- database.py
+++ database.py
@@ -2,10 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
+import json
 
-current_dir = os.path.dirname(__file__)
+f = open('secret.json', 'r')
+db = json.loads(f.read())
+f.close()
 
-engine = create_engine('sqlite:////{}/luckymember.db'.format(current_dir), convert_unicode=True)
+engine = create_engine('postgresql://{user}:{pwd}@{host}/{db}'.format(user=db.get('db_user'),
+                                                                      pwd=db.get('db_pwd'),
+                                                                      host=db.get('db_host'),
+                                                                      db=db.get('db_name')),
+                       convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          expire_on_commit=False,
